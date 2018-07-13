98286d88b03228c36bae0dc61e8e4542edc558e9
sk0g <ani.mehta@mail.com>
2018-07-13 11:48:25 +1000

    First code commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Griffith-3813ICT-sk0g
-Software Frameworks subject files (node.js)
---
+++ WS1/index.js
@@ -0,0 +1,15 @@
+console.log("Hello World, Node.js!")
+
+var myString = "Hello@World, Node.js Stringsss"
+var badWord  = "bad word"
+var mistype  = "Spwlling"
+
+myString = myString.replace("@", " ")
+myString = myString.substring(0, myString.lastIndexOf("ss"))
+console.log(myString)
+
+if (badWord === "bad word") {
+    badWord = "****"
+}
+
+console.log(badWord)
