83f974a83c21b17f3f84469f34a4dba5c2337361
sk0g <ani.mehta@mail.com>
2018-07-20 10:04:52 +1000

    Add missing semi-colons to index.js

--- WS1/index.js
+++ WS1/index.js
@@ -1,15 +1,15 @@
-console.log("Hello World, Node.js!")
+console.log("Hello World, Node.js!");
 
-var myString = "Hello@World, Node.js Stringsss"
-var badWord  = "bad word"
-var mistype  = "Spwlling"
+var myString = "Hello@World, Node.js Stringsss";
+var badWord  = "bad word";
+var mistype  = "Spwlling";
 
 myString = myString.replace("@", " ")
-myString = myString.substring(0, myString.lastIndexOf("ss"))
-console.log(myString)
+myString = myString.substring(0, myString.lastIndexOf("ss"));
+console.log(myString);
 
 if (badWord === "bad word") {
-    badWord = "****"
+    badWord = "****";
 }
 
-console.log(badWord)
+console.log(badWord);
