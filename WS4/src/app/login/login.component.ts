1eaee9fdc85913fc66dec629fc6d444cd9d4f3df
sk0g <ani.mehta@mail.com>
2018-08-03 01:53:57 +1000

    Login logic now works

--- WS4/src/app/login/login.component.ts
+++ WS4/src/app/login/login.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
+    username:string = '';
+    password:string = '';
+    validLogins:Array<[string, string]> = [
+        ['Allan', '123'],
+        ['admin', 'password'],
+        ['asd', '123']
+    ]
 
-    constructor() { }
+    constructor(private router:Router, private forms:FormsModule) {
 
-    ngOnInit() {
     }
 
+    ngOnInit() {
+
+    }
+
+    loginUser(event) {
+        event.preventDefault();
+
+        this.validLogins.forEach(element => {
+            if (this.username == element[0] &&
+                this.password == element[1]) {
+                    this.router.navigateByUrl('/account');
+                }
+        });
+    }
 }
